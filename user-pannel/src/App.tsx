ef36572ba38ace5dbaa257fc80ca35d22be14736
Elham <elham.yadegary@gmail.com>
2023-08-04 22:15:34 +0330

    fix structure and tailwind

--- user-pannel/src/App.tsx
+++ user-pannel/src/App.tsx
@@ -3,8 +3,9 @@ import { Login } from './Components/Login/login';
 
 function App() {
       return (
-            <div className="App">
-                  <Login />
+            <div className="App container">
+                  {/* <Login /> */}
+                  <h1 className="text-4xl font-bold">hello text</h1>
             </div>
       );
 }
--- user-pannel/src/Components/Login/login.tsx
+++ user-pannel/src/Components/Login/login.tsx
@@ -8,13 +8,18 @@ export const Login = () => {
             <>
                   <div>
                         <label id="username">نام کاربری :</label>
-                        <input></input>
+                        <input className="border border-gray-100"></input>
                   </div>
                   <div>
                         <label>رمز عبور :</label>
-                        <input></input>
+                        <input className="border border-gray-100"></input>
                   </div>
-                  <button onClick={handleClickLoginButton}>ورود</button>
+                  <button
+                        onClick={handleClickLoginButton}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4"
+                  >
+                        ورود
+                  </button>
             </>
       );
 };
