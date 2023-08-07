7070782110633f4822d57fb7cb217c40c32e01d7
yadegari.e <yadegari.e@tiddev.com>
2023-08-07 10:56:26 +0330

    add event types

--- user-pannel/src/Components/Login/login.tsx
+++ user-pannel/src/Components/Login/login.tsx
@@ -1,34 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleClickLoginButton = () => {
     console.log("login");
   };
+
+  const handleBlurUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(event.target.value);
+  };
+
+  const handleBlurPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center w-2/5 mx-auto my-52 border border-slate-300 p-16">
-      <div className="">
-        <label id="username" className="text-center">
-          نام کاربری :
-        </label>
-        <input
-          tabIndex={0}
-          className="border border-gray-100 border-slate-300"
-        ></input>
+    <div className="container w-2/5 mx-auto my-52 border border-slate-300 p-14 rounded">
+      <div className="container columns-3 mb-3">
+        <div className="columns-6">
+          <label id="username" className="block w-40 text-right p-2">
+            نام کاربری :
+          </label>
+        </div>
+        <div className="columns-6">
+          <input
+            name="username"
+            tabIndex={0}
+            className="border border-gray-100 border-slate-300 rounded p-2"
+            onBlur={handleBlurUserName}
+          />
+        </div>
       </div>
-      <div>
-        <label className="text-center">رمز عبور :</label>
-        <input
-          tabIndex={1}
-          className="border border-gray-100 mt-11 border-slate-300"
-        ></input>
+      <div className="container columns-3 mb-3">
+        <div className="columns-6">
+          <label className="block w-40 text-right p-2">رمز عبور :</label>
+        </div>
+        <div className="columns-6">
+          <input
+            tabIndex={1}
+            className="border border-gray-100 border-slate-300 rounded p-2"
+            onBlur={handleBlurPassword}
+          />
+        </div>
       </div>
-      <button
-        tabIndex={2}
-        onClick={handleClickLoginButton}
-        className="bg-blue-500 hover:bg-blue-700 text-white text-1xl font-bold py-2 px-4 border border-blue-700 rounded mt-10 w-2/5"
-      >
-        ورود
-      </button>
+      <div className="container flex justify-center columns-3 mt-12">
+        <button
+          tabIndex={2}
+          onClick={handleClickLoginButton}
+          className="bg-blue-500 hover:bg-blue-700 text-white text-1xl font-bold py-2 px-4 border border-blue-700 rounded w-2/5"
+        >
+          ورود
+        </button>
+      </div>
     </div>
   );
 };
